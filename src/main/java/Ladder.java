ea6e8c754d6d2eaef147ff929b65518173c3ecb5
mr8356 <mr8356@naver.com>
2024-09-16 17:17:28 +0900

    미션 화이팅

--- src/main/java/Ladder.java
+++ src/main/java/Ladder.java
@@ -1,8 +1,32 @@
-public class Ladder {
 
+public class Ladder {
+    // 저장되는 값은 j방향으로 이동해야되는 정도
     private final int[][] rows;
 
-    public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+    public Ladder(NaturalNumber row, NaturalNumber numberOfPerson) {
+        rows = new int[row.getNaturalNumber()+1][numberOfPerson.getNaturalNumber()+1];
+    }
+
+    public int drawLine(Position pos1 , Position pos2) {
+        if(pos1.getRow() != pos2.getRow()) {
+            throw new IllegalArgumentException("Positions do not match");
+        }
+        int row = pos1.getRow();
+        int distance = pos2.getCol() - pos1.getCol();
+        rows[row][pos1.getCol()] = distance;
+        rows[row][pos2.getCol()] = -1 * distance;
+        return 1;
+    }
+
+    public int run(NaturalNumber i) {
+        int currentCol = i.getNaturalNumber();
+        for (int j = 1; j < rows.length; j++) {
+            currentCol += rows[j][currentCol];
+        }
+        return currentCol;
+    }
+
+    public int getDistance(Position position) {
+        return rows[position.getRow()][position.getCol()];
     }
 }
---
+++ src/main/java/NaturalNumber.java
@@ -0,0 +1,17 @@
+public class NaturalNumber {
+    private int naturalNumber;
+
+    private NaturalNumber(int naturalNumber) {
+        if (naturalNumber < 1) {
+            throw new IllegalArgumentException("Invalid natural number");
+        }
+        this.naturalNumber = naturalNumber;
+    }
+    public static NaturalNumber from(int i) {
+        return new NaturalNumber(i);
+    }
+
+    public int getNaturalNumber() {
+        return naturalNumber;
+    }
+}
